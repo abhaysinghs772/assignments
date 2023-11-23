d2c477f8fe030b199108bbb66046bc3698a1aab9
abhaysinghs772 <abhaysinghs772@gmail.com>
2023-11-24 02:13:24 +0530

    i fixed the issue with connect my amazon-rds potgres to my local nest app connection and i also put pdf in this directory because i like this chalange and tasks mentioned in the assignment's pdf

--- tickete/src/app.module.ts
+++ tickete/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+      logging: true,
+      // thank you God finnally it worked, the line below saved me
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
